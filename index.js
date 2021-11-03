7f14f32a4b12e666abdaf3cfbea1a9ab014a4d8b
Akeem <AkeemS11788@gmail.com>
2021-11-03 19:13:30 -0400

    commit tree 48761de303ad3a3865cda9e4740eecc875442218

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(2, 4);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+    return function (fare) {
+        return fare * integer
+    }
+};
+
+// const kindaExpensive = createFareMultiplier
+// function fareDoubler(kindaExpensive) {
+//     return kindaExpensive * 2;
+// };
+const fareDoubler = createFareMultiplier(2)
+// const madExpensive = createFareMultiplier
+// function fareTripler(madExpensive) {
+//     return madExpensive * 3;
+// };
+const fareTripler = createFareMultiplier(3)
+function selectDifferentDrivers(drivers, returnLastTwoDrivers) {
+    return returnLastTwoDrivers (drivers);
+};
